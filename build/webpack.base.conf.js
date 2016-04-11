a4bf0816b7f3188227e707d8e914fa58dd823d5f
Song,Jia <songjia.happycoding@gmail.com>
2016-04-11 20:54:57 +0800

    construct directorys
    use express build server side and use vuejs build client side.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,7 +5,7 @@ var projectRoot = path.resolve(__dirname, '../')
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './client/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -16,9 +16,9 @@ module.exports = {
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, '../node_modules')],
     alias: {
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'src': path.resolve(__dirname, '../client'),
+      'assets': path.resolve(__dirname, '../client/assets'),
+      'components': path.resolve(__dirname, '../client/components')
     }
   },
   resolveLoader: {

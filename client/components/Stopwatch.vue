072a1a816f2e42fbdad2a902f09e83d983514e0f
Song,Jia <jsong@nltechdev.com>
2016-07-05 16:35:49 +0800

    implement actions of start, pause, complete buttons. #17

---
+++ client/components/Stopwatch.vue
@@ -0,0 +1,64 @@
+<template>
+  <span>{{ time }}</span>
+</template>
+
+<script>
+  /*
+   * count time and display time in format h : m : s.
+   * automatically start counting when this compenent has been created.
+   * properties:
+   * > startTime: default to current timestamp.
+   */
+  export default {
+    data () {
+      return {
+        time: ''
+      }
+    },
+    props: {
+      startTime: {
+        type: Number,
+        default: (new Date()).getTime()
+      }
+    },
+    methods: {
+      updateTime () {
+        this.time = this.formatTime((new Date()).getTime() - this.startTime)
+      },
+      /**
+       * format time stamp to format 00:00:00
+       */
+      formatTime (time) {
+        time = Math.floor(time / 1000)
+        var s = time % 60
+        var m = Math.floor(time / 60)
+        var h = Math.floor(time / 3600)
+        return `${this.paddingZero(h)}:${this.paddingZero(m)}:${this.paddingZero(s)}`
+      },
+      /**
+       * padding zero to left of number string.
+       */
+      paddingZero (n) {
+        n = String(n)
+        if (n.length < 2) {
+          return '0' + n
+        } else {
+          return n
+        }
+      }
+    },
+    /**
+     * set interval for updating time every second.
+     */
+    created () {
+      this.updateTime()
+      this.interval = window.setInterval(this.updateTime, 1000)
+    },
+    /**
+     * remove interval when component is destroied.
+     */
+    destroyed () {
+      window.clearInterval(this.interval)
+    }
+  }
+</script>

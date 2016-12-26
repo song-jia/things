761fc1c2c02c879be39c75afaa320e103e5b732a
Song,Jia <songjia.happycoding@gmail.com>
2016-12-26 22:42:21 +0800

    #23 使用validator校验登录输入。

--- client-react/src/routes/Login/components/LoginForm.js
+++ client-react/src/routes/Login/components/LoginForm.js
@@ -1,31 +1,61 @@
 import React, {Component} from 'react'
 import { Row, Col, Form, Icon, Input, Button } from 'antd'
 import styles from './loginForm.css'
+import validator from 'validator'
+
 const FormItem = Form.Item
 
 class LoginForm extends Component {
-  // handleSubmit(e) {
-  //   e.preventDefault();
-  //   this.props.form.validateFields((err, values) => {
-  //     if (!err) {
-  //       console.log('Received values of form: ', values);
-  //     }
-  //   });
-  // },
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: '',
+      userError: false,
+      password: '',
+      passwordError: false
+    }
+  }
+  /**
+   * 校验用户名，密码，提交登录。
+   */
+  handleSubmit (e) {
+    e.preventDefault()
+    if (this.validateFields()) {
+      console.log('request post')
+    }
+  }
+
   render () {
     return (
       <Row type='flex' align='middle' justify='center' className={styles.content}>
         <Col xs={2} sm={4} md={6} lg={8} className={styles.section}>
           <h1 className={styles.title}>Things</h1>
-          <Form onSubmit={this.handleSubmit}>
-            <FormItem>
-              <Input addonBefore={<Icon type='user' />} placeholder='Username' />
+          <Form onSubmit={(e) => this.handleSubmit(e)}>
+            <FormItem
+              validateStatus={this.state.userError ? 'error' : ''}
+              help={this.state.userError ? '请输入用户名。' : ''}>
+              <Input
+                addonBefore={<Icon type='user' />}
+                placeholder='用户名'
+                value={this.state.user}
+                onChange={(e) => this.setState({user: e.target.value})} />
+            </FormItem>
+            <FormItem
+              validateStatus={this.state.passwordError ? 'error' : ''}
+              help={this.state.passwordError ? '请输入密码。' : ''}>
+              <Input
+                addonBefore={<Icon type='lock' />}
+                type='password'
+                placeholder='密码'
+                value={this.state.password}
+                onChange={(e) => this.setState({password: e.target.value})} />
             </FormItem>
             <FormItem>
-              <Input addonBefore={<Icon type='lock' />} type='password' placeholder='Password' />
-            </FormItem>
-            <FormItem>
-              <Button type='primary' htmlType='submit' className='login-form-button'>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='login-form-button'>
                 登录
               </Button>
             </FormItem>
@@ -33,6 +63,15 @@ class LoginForm extends Component {
         </Col>
       </Row>
     )
+  }
+
+  validateFields () {
+    let userError = validator.isEmpty(this.state.user)
+    let passwordError = validator.isEmpty(this.state.password)
+    this.setState({
+      userError: userError,
+      passwordError: passwordError})
+    return userError && passwordError
   }
 }
 
